2284bac0b802040565a6695dd923d365a78d2ce2
Al Emran <alemrangazi@gmail.com>
2024-08-17 16:01:11 +0600

    content,important,links,nav

--- src/ImportantArea.js
+++ src/ImportantArea.js
@@ -1,14 +1,63 @@
 import "./ImportantArea.css";
-
+import ImportantLinkItem from "./ImportantLinkItem";
 const ImportantArea = () => {
-    return (
-        <div className="important-area">
-            {/* links */}
-            <div className="important-links"></div>
-            {/* notice board */}
-            <div className="notice-board"></div>
-        </div>
-    );
+  return (
+    <div className="important-area">
+      {/* links */}
+      <ImportantLinkItem
+        logo={
+          "https://www.police.gov.bd/storage/upload/announcement/WfQLLDptPSdlOI8kNu4BnUoV4uXZSAX7prmWByC8.png"
+        }
+        title={"Online Police Clearance"}
+      />
+      <ImportantLinkItem
+        logo={
+          "https://www.police.gov.bd/storage/upload/announcement/tKm76RLPfdM4sj2z4GRRhtyfAKktsmgXGhiE7FFM.png"
+        }
+        title={"BD Police Helpline"}
+      />
+      <ImportantLinkItem
+        logo={
+          "https://www.police.gov.bd/storage/upload/announcement/T7CDr3HFARPHrnk9FeO5ceXeaa1blW3a00KsTGBz.png"
+        }
+        title={"Expatriate Cell"}
+      />
+      <ImportantLinkItem
+        logo={
+          "https://www.police.gov.bd/storage/upload/announcement/fOQH3PEvuFC69VHqe3ySxauebrDFiR4ZXv7h7itC.png"
+        }
+        title={"Opinion or Complaint "}
+      />
+      <ImportantLinkItem
+        logo={
+          "https://www.police.gov.bd/storage/upload/announcement/pSIACGowrQenbQABMsfpl9rHrYyBUcMoaMsWOLKg.png"
+        }
+        title={"Hotline Number"}
+      />
+      <ImportantLinkItem
+        logo={
+          "https://www.police.gov.bd/storage/upload/announcement/bURQrA0cBzIbT44WusDCAHV56wEKwqfAPpOcJjPn.png"
+        }
+        title={"PIMS"}
+      />
+      <ImportantLinkItem
+        logo={
+          "https://www.police.gov.bd/storage/upload/announcement/cSIdRQWCQgZJCxORAYaaBpuU2Y601S4cSgtirJ9N.png"
+        }
+        title={"BPWN Annual Training"}
+      />
+      <ImportantLinkItem
+        logo={
+          "https://www.police.gov.bd/storage/upload/announcement/qlck9RMnvTW7ibW37f1u4CUxW0nyNX3qyfxcqtgJ.png"
+        }
+        title={"Achievment & Success"}
+      />
+      
+
+      {/* notice board */}
+      <div className="notice-board"></div>
+    </div>
+  );
 };
 
 export default ImportantArea;
---
+++ src/ImportantLinkItem.js
@@ -0,0 +1,16 @@
+const ImportantLinkItem = ({logo, title}) => {
+    return (
+        <div className="important-links">
+        <a href="" className="item">
+          <div className="img-container">
+            <img
+              src= {logo}
+              alt=""
+            />
+          </div>
+          <p>{title}</p>
+        </a>
+      </div>
+    );
+};
+export default ImportantLinkItem;
