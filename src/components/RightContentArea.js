f0a14081b5fc5e39f6123300356d6cc02d61af35
Al Emran <alemrangazi@gmail.com>
2024-08-21 23:04:31 +0600

    fonts updated

--- src/components/RightContentArea.js
+++ src/components/RightContentArea.js
@@ -5,11 +5,17 @@ import { importantLinkDataRight } from "../importantLinkData";
 
 const RightContentArea = () => {
   return (
-    <div className="right-content-area">
-      {
-      importantLinkDataRight.map(item => 
-        <ImportantLinkRight key={item.id} logo={item.img} title={item.title} />
-      )}
+    <div className="total-right">
+      <div className="right-content-area-top">
+        {importantLinkDataRight.map((item) => (
+          <ImportantLinkRight
+            key={item.id}
+            logo={item.img}
+            title={item.title}
+          />
+        ))}
+      </div>
+      <div className=""></div>
     </div>
   );
 };
